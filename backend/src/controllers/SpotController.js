27abe9dfcd8d627cd66ad846174a0de784f565ae
Larissa <larissa.silva@cubos.io>
2019-10-07 10:45:47 -0300

    feat: store spot

--- backend/src/controllers/SpotController.js
+++ backend/src/controllers/SpotController.js
@@ -1,5 +1,5 @@
-const User = require('../models/User');
-const Spot = require('../models/Spot');
+const User = require("../models/User");
+const Spot = require("../models/Spot");
 
 module.exports = {
   async index(req, res) {
@@ -13,20 +13,23 @@ module.exports = {
     const { filename } = req.file;
     const { company, price, techs } = req.body;
     const { user_id } = req.headers;
-    
-    const user = await User.findById(user_id);
-    if(!user){
-      return res.status(400).json({ error: 'User does not exists '});
+
+    let user = await User.findById(user_id);
+    if (!user) {
+      return res.status(400).json({ error: "User does not exists " });
     }
-    
-    const spot = await Spot.create({
-      user: user_id,
-      thumbnail: filename,
-      company,
-      techs: techs.split(',').map(tech => tech.trim()),
-      price
-    });
-  
+
+    let spot = await Spot.findOne({ company });
+    if (!spot) {
+      spot = await Spot.create({
+        user: user_id,
+        thumbnail: filename,
+        company,
+        techs: techs.split(",").map(tech => tech.trim()),
+        price
+      });
+    }
+
     return res.json(spot);
   }
 };
