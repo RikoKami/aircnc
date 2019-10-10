7126d42e016667cc133fc218520777a2df556308
Larissa <larissa.silva@cubos.io>
2019-10-10 12:44:02 -0300

    Dashboard: Parametros em ordem para listagem de spots

--- backend/src/controllers/DashboardController.js
+++ backend/src/controllers/DashboardController.js
@@ -4,10 +4,10 @@ const Spot = require("../models/Spot");
  * Perfil do usuário
  */
 module.exports = {
-  async show(res,req) {
+  async show(req, res) {
     const { user_id } = req.headers;
     const spots = await Spot.find({ user: user_id });
 
     return res.json(spots);
   }
-}
+};
